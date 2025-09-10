840f8e38c043a5544972defcb876969adc65b999
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-09-10 12:11:25 +0300

    Add project files.

---
+++ Session 02/Program.cs
@@ -0,0 +1,11 @@
+﻿using static Session_02.ListGenerator;
+namespace Session_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

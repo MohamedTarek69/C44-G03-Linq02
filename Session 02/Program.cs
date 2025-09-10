1e9d130ead8f8183c32833af7da4f82a5cf15cb1
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-09-10 12:18:23 +0300

    Casting Operators - Immediate Execution

--- Session 02/Program.cs
+++ Session 02/Program.cs
@@ -1,11 +1,49 @@
 ﻿using static Session_02.ListGenerator;
+using Session_02.Data;
+using System.Collections;
 namespace Session_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            //--------------------------------------- Vedio 02 ---------------------------------------//
+            #region Casting Operators - Immediate Execution  
+
+            //List<Product> Results = ProductList.Where(P => P.UnitsInStock == 0).ToList();
+
+            //Product[] Result = ProductList.Where(P => P.UnitsInStock == 0).ToArray();
+
+            //Dictionary<long, string> Result = ProductList.Where(P => P.UnitsInStock == 0).ToDictionary(P => P.ProductID, P => P.ProductName);
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            //foreach (var item in Result)
+            //    Console.WriteLine($"Key = {item.Key} , Value = {item.Value}");
+
+            //HashSet<Product> Result = ProductList.Where(P => P.UnitsInStock == 0).ToHashSet();
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            //ArrayList arrayList = new ArrayList()
+            //    {
+            //        "Ahmed",
+            //        "Mona",
+            //        "Aya",
+            //        1,
+            //        2,
+            //        3
+            //    };
+
+            //var Result = arrayList.OfType<int>();
+
+            //foreach (var item in Result)
+            //    Console.WriteLine(item);
+
+            #endregion
+
         }
     }
 }
